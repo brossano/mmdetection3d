a9ba43ca501aac99adfab9c5c419a302e7b9ffa2
kvishnu <kvishnu@umich.edu>
2023-10-29 21:12:19 -0400

    added loss as module

--- mmdet3d/models/losses/smooth_l1_loss_uncertainty.py
+++ mmdet3d/models/losses/smooth_l1_loss_uncertainty.py
@@ -174,3 +174,52 @@ class L1Loss(nn.Module):
         loss_bbox = self.loss_weight * l1_loss(
             pred, target, weight, reduction=reduction, avg_factor=avg_factor)
         return loss_bbox
+    
+@MODELS.register_module()
+class NegativeLogPDFLoss(nn.Module):
+    """Negative Log(PDF) loss.
+
+    Args:
+        reduction (str, optional): The method to reduce the loss.
+            Options are "none", "mean" and "sum".
+        loss_weight (float, optional): The weight of loss.
+    """
+
+    def __init__(self,
+                 reduction: str = 'mean',
+                 loss_weight: float = 1.0) -> None:
+        super().__init__()
+        self.reduction = reduction
+        self.loss_weight = loss_weight
+
+    def forward(self,
+                pred: Tensor,
+                target: Tensor,
+                weight: Optional[Tensor] = None,
+                avg_factor: Optional[int] = None,
+                reduction_override: Optional[str] = None) -> Tensor:
+        """Forward function.
+
+        Args:
+            pred (Tensor): The prediction.
+            target (Tensor): The learning target of the prediction.
+            weight (Tensor, optional): The weight of loss for each
+                prediction. Defaults to None.
+            avg_factor (int, optional): Average factor that is used to average
+                the loss. Defaults to None.
+            reduction_override (str, optional): The reduction method used to
+                override the original reduction method of the loss.
+                Defaults to None.
+
+        Returns:
+            Tensor: Calculated loss
+        """
+        # if weight is not None and not torch.any(weight > 0):
+        #     if pred.dim() == weight.dim() + 1:
+        #         weight = weight.unsqueeze(1)
+        #     return (pred * weight).sum()
+        # assert reduction_override in (None, 'none', 'mean', 'sum')
+        # reduction = (
+        #     reduction_override if reduction_override else self.reduction)
+        loss_bbox = self.loss_weight * neg_log_pdf_loss(pred, target)
+        return loss_bbox
