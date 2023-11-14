1dcfeb2c622dc67bfa0413508c7fc364bd9cc579
kvishnu <kvishnu@umich.edu>
2023-11-14 17:40:47 -0500

    changed loss_dir for anchor head back to cross entropy

--- mmdet3d/models/dense_heads/anchor3d_head_uncertainty.py
+++ mmdet3d/models/dense_heads/anchor3d_head_uncertainty.py
@@ -73,17 +73,17 @@ class Anchor3DHeadUncertainty(Base3DDenseHead, AnchorTrainMixin):
                      use_sigmoid=True,
                      loss_weight=1.0),
                 loss_bbox: ConfigType = dict(
-                    type='mmdet.NegativeLogPDFLoss',
+                    type='mmdet3d.NegativeLogPDFLoss',
                     loss_weight=2.0),
                 #  loss_bbox: ConfigType = dict(
                 #     type='mmdet.SmoothL1Loss',
                 #      beta=1.0 / 9.0,
                 #      loss_weight=2.0),
                 loss_dir: ConfigType = dict(
-                    type='mmdet.NegativeLogPDFLoss',
-                    loss_weight=0.2),
-                #  loss_dir: ConfigType = dict(
-                #      type='mmdet.CrossEntropyLoss', loss_weight=0.2),
+                     type='mmdet.CrossEntropyLoss', loss_weight=0.2),
+                # dict(
+                #     type='mmdet3d.NegativeLogPDFLoss',
+                #     loss_weight=0.2),
                  train_cfg: OptConfigType = None,
                  test_cfg: OptConfigType = None,
                  init_cfg: OptConfigType = None) -> None:
